d604de3ff725d700e4d8b0bf47cd5d9b95cf0ab4
Grandmasterkim <139945901+Grandmasterkim@users.noreply.github.com>
2023-07-28 15:53:54 +0100

    main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 
 #loading the saved model
-loaded_model = pickle.load(open('C:/Users/Masterkim/projects/BMI/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 
 #create a function for prediction
